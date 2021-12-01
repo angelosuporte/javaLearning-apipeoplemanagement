f7bd6524224d6b9bdb2f18fccc31130cdd473869
Angelo Carvalho <angelosuporteti@gmail.com>
2021-12-01 18:33:49 -0300

    Refactoring method the catch exception

--- personapi/src/main/java/com/challenge/personapi/service/PersonService.java
+++ personapi/src/main/java/com/challenge/personapi/service/PersonService.java
@@ -2,6 +2,7 @@ package com.challenge.personapi.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,10 +45,10 @@ public class PersonService {
 	}
 
 	public PersonDTO findById(Long id) throws PersonNotFoundException{
-		Optional<Person> optionalPerson = personRepository.findById(id);
-		if (optionalPerson.isEmpty()){
-			throw new PersonNotFoundException(id);
-		}
-		return personMapper.toDTO(optionalPerson.get());
+		Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+		//Optional<Person> optionalPerson = personRepository.findById(id);
+		//if (optionalPerson.isEmpty()){
+			//throw new PersonNotFoundException(id);
+		return personMapper.toDTO(person);
 	}
 }
